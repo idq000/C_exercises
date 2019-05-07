0626a412c59a55b1ee5f1f2967d301a914747e9f
idq000 <idq000a@gmail.com>
2019-05-07 16:06:57 -0500

    Exercise 3: Modified makefile to include all other exercises; also added previous exercises to folder

---
+++ ex3/ex2_1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+	
+	//this is also a comment
+	printf("You are %d miles away.\n", distance);
+	
+	distance--;
+	
+	//this one is for william
+	printf("There are %d weeaboos but there is only one sensei\n",distance);
+	
+	distance--;
+	
+	//this one is for collie
+	printf("William has %d dollars, but he always says he's broke\n", distance);
+	
+	distance--;
+	
+	//make me a mod plz
+	printf("Why is this exercise so %d boring %d\n", distance, distance);
+	
+	distance--;
+	
+	//this is the worst kthxbye
+	printf("This is the last sentence finally %d\n", distance);
+	
+	return 0;
+}
