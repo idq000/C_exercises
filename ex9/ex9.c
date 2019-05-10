3eb7728281def993a949fd65b9a61dfadbbb5397
idq000 <idq000a@gmail.com>
2019-05-10 18:24:10 -0500

    Exercise 9: Count from 25 to 0

--- ex9/ex9.c
+++ ex9/ex9.c
@@ -2,11 +2,11 @@
 
 int main (int argc, char *argv[])
 {
-	int i = 0;
+	int i = 25;
 	
-	while (i < 25){
+	while (i >= 0){
 		printf("%d", i);
-		i++;
+		i--;
 	}
 	
 	// need this to add a final newline
