148f65dd3236ac8949c81784af1be75dba5e0397
idq000 <idq000a@gmail.com>
2019-05-10 18:15:13 -0500

    Exercise 8: EC: Fixed for no argumments and other cases

--- ex8/ex8.c
+++ ex8/ex8.c
@@ -3,13 +3,15 @@
 int main(int argc, char *argv[])
 {
 	int i = 0;
-	
-	if (argc == 1) {
+	argc--;
+	if (argc == 0) {
+		printf("No arguments entered.  Good job!\n");
+	} else if (argc == 1) {
 		printf("You only have one argument.  You suck.\n");
 	} else if (argc > 1 && argc < 4) {
 		printf("Here's your arguments:\n");
 		
-		for (i = 0; i < argc; i++){
+		for (i = 0; i <= argc; i++){
 			printf(" %s",argv[i]);
 		}
 		printf("\n");
