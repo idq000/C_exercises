5bc2a95fbacef1cf55aeb857e21e0fc5c390e459
idq000 <idq000a@gmail.com>
2019-05-10 18:35:06 -0500

    super rich n fly

--- ex9/ex9.c
+++ ex9/ex9.c
@@ -12,5 +12,18 @@ int main (int argc, char *argv[])
 	// need this to add a final newline
 	printf("\n");
 	
+	i = 1;
+	
+	while (i <= 10){
+		
+		if (i == 1){
+			printf("William has a dolla supa rich n fly\n");
+		} else {
+			printf("William has $%d million dollars in the bank.\n",i);
+		}
+		i++;
+		
+	}
+	
 	return 0;
 }
